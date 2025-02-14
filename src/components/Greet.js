df5fdaa94393e9ee37089ef0b9dc46090747ee78
pandukayalaganga <146313572+pgpandu574@users.noreply.github.com>
2025-02-14 14:09:03 +0530

    Initial commit

---
+++ src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// pascalcase 
+function Greet(){
+
+    return <h1>Happy married life srihan
+        stay happy where ever you. iam with u in your happyness and sadness <br></br>
+        once again congras and happy married life and happy valentine day raye srihan  </h1>
+
+}
+
+export default Greet;
